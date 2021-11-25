7b67f3b4105fb6aaced6aa987cdc6c01da5aa106
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2021-11-25 14:06:49 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <style>
+        
+        #logo {
+            background-color: green;
+            transition: transform .2s;
+            /* Animation */
+            width: 80px;
+            height: 80px;
+            margin: 0 auto;
+        }
+        #logo:hover {
+            transform: scale(1.5);
+        }
+    </style>
+</head>
+<body>
+    <div class="title">
+        
+    <a href="dashboard.php"><img src="./images/cutm.png" alt="CUTM" id="logo"></a>
+
+        <span> <big> Welcome To CUTM </big></span>
+        <br>
+        <span><big> Student Result Management System </big></span>
+    </div>
+
+    <div class="main">
+        <div class="login">
+            <form action="" method="post" name="login">
+                <fieldset>
+                    <legend class="heading"><big> <b>Admin Login</big></b> </legend>
+                    <input type="text" name="userid" placeholder="Email" autocomplete="off">
+                    <input type="password" name="password" placeholder="Password" autocomplete="off">
+                    <input type="submit" value="Login">
+                </fieldset>
+            </form>    
+        </div>
+        <div class="search">
+            <form action="./student.php" method="get">
+                <fieldset>
+                    <legend class="heading"><big> <b> For Students</big></b></legend>
+
+                    <?php
+                        include('init.php');
+
+                        $class_result=mysqli_query($conn,"SELECT `name` FROM `class`");
+                            echo '<select name="class" style="color: rgb(228 255 0)" >';
+                            echo '<option selected disabled>Select Class</option>';
+                        while($row = mysqli_fetch_array($class_result)){
+                            $display=$row['name'];
+                            echo '<option value="'.$display.'">'.$display.'</option>';
+                        }
+                        echo'</select>'
+                    ?>
+
+                    <input type="text" name="rn" placeholder="Roll No">
+                    <input type="submit" value="Get Result">
+                </fieldset>
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
+
+<?php
+    include("init.php");
+    session_start();
+
+    if (isset($_POST["userid"],$_POST["password"]))
+    {
+        $username=$_POST["userid"];
+        $password=$_POST["password"];
+        $sql = "SELECT userid FROM admin_login WHERE userid='$username' and password = '$password'";
+        $result=mysqli_query($conn,$sql);
+
+        // $row=mysqli_fetch_array($result);
+        $count=mysqli_num_rows($result);
+        
+        if($count==1) {
+            $_SESSION['login_user']=$username;
+            header("Location: dashboard.php");
+        }else {
+            echo '<script language="javascript">';
+            echo 'alert("Invalid Username or Password")';
+            echo '</script>';
+        }
+        
+    }
+?>
+
